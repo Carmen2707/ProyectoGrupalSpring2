5f1f96404570664d39def7d7674c1cd9a6c86bff
Raulha9 <Raulajm07@gmail.com>
2024-02-23 17:43:47 +0100

    Añadir el Utility

---
+++ src/main/java/com/example/proyectogrupalspring2/util/Utility.java
@@ -0,0 +1,20 @@
+package com.example.proyectogrupalspring2.util;
+
+import java.io.IOException;
+
+public class Utility {
+    public static void launchWeb(){
+        String system = System.getProperty("system.name").toLowerCase();
+        try{
+            if (system.contains("win")){
+                Runtime.getRuntime().exec("cmd /c start chrome http://localhost:8080/login");
+            }else{
+                Runtime.getRuntime().exec("cmd -a start Safari http://localhost:8080/login");
+
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
